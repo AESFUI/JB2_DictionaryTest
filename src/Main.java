c2429c95c8c514663fe09f12705a72fa5c45c14c
AESFUI <vierdas@gmail.com>
2016-11-06 15:30:42 +0300

    .properties перенесены в папку resources

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите название языка для тестирования (deutsch/english/espanol): ");
+
 
     }
 }
