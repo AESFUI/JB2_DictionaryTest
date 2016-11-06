e07dc263c7de482c80679ed9fccd087ae92eee43
AESFUI <vierdas@gmail.com>
2016-11-06 17:49:33 +0300

    Уже что-то по коду и с помощью native2ascii привёл содержимое .properies к правильной кодировке

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Properties;
+import java.util.*;
 
 public class Main {
 
@@ -16,7 +16,7 @@ public class Main {
         String dictionaryName = "";
         try {
             dictionaryName = reader.readLine();
-            reader.close();
+            //reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -45,18 +45,45 @@ public class Main {
         try {
             fis = new FileInputStream("src/resources/" + dictionaryName + ".properties");
             property.load(fis);
-
-            /*String host = property.getProperty("db.host");
-            String login = property.getProperty("db.login");
-            String password = property.getProperty("db.password");
-
-            System.out.println("HOST: " + host
-                    + ", LOGIN: " + login
-                    + ", PASSWORD: " + password);*/
-
         } catch (IOException e) {
             System.err.println("ОШИБКА: Не удается загрузить тест!");
         }
+
+        Set<String> dictionarySet = property.stringPropertyNames();
+        String[] dictionaryStringArray = {};
+        dictionaryStringArray = dictionarySet.toArray(new String[dictionarySet.size()]);
+
+        ArrayList<String> dictionaryList = new ArrayList<String>(Arrays.asList(dictionaryStringArray));
+
+        String entering = "";
+        int yes = 0, no = 0;
+
+        try {
+            while (!dictionaryList.isEmpty()) {
+                String word = dictionaryList.get((int) Math.random() * dictionaryList.size()); //случайный индекс
+                System.out.print("Напишите перевод слова " + word + ": ");
+
+                entering = reader.readLine();
+                System.out.println();
+
+                if (entering.equals("exit")) {
+                    break;
+                    //вывод статистики на экран
+                    // запрос пути, куда записать статистику
+                } else if (entering.equals(property.getProperty(entering))) {
+                    yes++;
+                } else {
+                    no++;
+                }
+
+                dictionaryList.remove(word);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Правильных ответов: " + yes);
+        System.out.println("Ошибок: " + no);
 
 
     }
